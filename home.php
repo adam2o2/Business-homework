6184ed100bba436b51f62f68267dab3a94f12289
adam2o2 <78441732+adam2o2@users.noreply.github.com>
2023-01-19 09:15:10 -0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--    Author:     Mike O'Kane
+        Date:        August 15, 2007
+        File:        my-web4.html
+        Purpose:     HTML Example - using a different style sheet
+-->
+
+<html>
+<head>
+    <title>HTML Example</title>
+    <link rel ="stylesheet" type="text/css" href="funky.css">
+</head>
+<body>
+    <h1>Chattanooga Printing Company</h1>
+    <p>By: Adam May, Dylan Quarles</p>
+
+    <img src = "dude.jpg" alt = "image of alpha male">
+
+    <p>This is the main page of <strong>Chattanooga Printing Company</strong>. let us tell you little about what we do.</p>
+    <p>Show your school spirit and support your favorite team. Shop now at UTC bookstore. Limited time only!</p>
+
+    <p>We strive to get the best prices and clothing and prints if you have any questions give us a call at 423-123-3122 or email us at pol099@mocs.utc.edu click here to get more pricing information</p>
+    <p>
+    <a href = "http://www.w3c.org/%22%3EThe World Wide Web Consortium</a> <br>
+    <a href = "http://www.php.net/%22%3EPHP Home Page</a> <br>
+    <a href = "http://www.w2schools.com/%22%3EW3Schools - Programming Tutorials</a>
+    <br>
+    </p>
+    <p>
+    <a href = "welcome.html"></a> <br>
+    <a href = "quote-generator.php"></a> <br>
+    </p>
+
+
+<style>
+table.GeneratedTable {
+  width: 20%;
+  background-color: #ffffff;
+  border-collapse: collapse;
+  border-width: 2px;
+  border-color: #0008ff;
+  border-style: solid;
+  color: #000000;
+}
+
+table.GeneratedTable td, table.GeneratedTable th {
+  border-width: 2px;
+  border-color: #0008ff;
+  border-style: solid;
+  padding: 3px;
+}
+
+table.GeneratedTable thead {
+  background-color: #ffcc00;
+}
+</style>
+
+
+<table class="GeneratedTable">
+  <thead>
+    <tr>
+      <th></th>
+      <th>Black</th>
+      <th>Blue</th>
+      <th>White</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Long-sleeve</td>
+      <td>19$</td>
+      <td>17$</td>
+      <td>15$</td>
+    </tr>
+    <tr>
+      <td>T-shirt</td>
+      <td>14$</td>
+      <td>12$</td>
+      <td>10$</td>
+    </tr>
+    <tr>
+      <td>Jacket</td>
+      <td>34$</td>
+      <td>32$</td>
+      <td>30$</td>
+    </tr>
+  </tbody>
+</table>
+<a href="loop.php">For more information</a>
+
+</body>
+</html>
---
+++ loop.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+    <title>HTML Example</title>
+    <link rel ="stylesheet" type="text/css" href="funky.css">
+</head>
+<body>
+    <h1>Chattanooga Printing Company</h1>
+    <p>By: Adam May, Dylan Quarles</p>
+
+
+    <img src = "shirt.jpg" alt = "image of different color shirts">
+
+    <p>Prices and availability are subject to change, so to ensure your order is fulfilled properely please call us toll free at 423-123-3122!</p>
+    <p>We strive to ensure that our customers are happy and satisfied with their purchases in every way possible.</p>
+
+    <p></p>
+    <p>
+    <a href = "http://www.w3c.org/%22%3EThe World Wide Web Consortium</a> <br>
+    <a href = "http://www.php.net/%22%3EPHP Home Page</a> <br>
+    <a href = "http://www.w2schools.com/%22%3EW3Schools - Programming Tutorials</a>
+    <br>
+    </p>
+    <p>
+    <a href = "welcome.html"></a> <br>
+    <a href = "quote-generator.php"></a> <br>
+    </p>
+    <style>
+table.GeneratedTable {
+  width: 20%;
+  background-color: #ffffff;
+  border-collapse: collapse;
+  border-width: 2px;
+  border-color: #0008ff;
+  border-style: solid;
+  color: #000000;
+}
+
+table.GeneratedTable td, table.GeneratedTable th {
+  border-width: 2px;
+  border-color: #0008ff;
+  border-style: solid;
+  padding: 3px;
+}
+
+table.GeneratedTable thead {
+  background-color: #ffcc00;
+}
+</style>
+
+
+<table class="GeneratedTable">
+  <thead>
+    <tr>
+      <th></th>
+      <th>Black</th>
+      <th>Blue</th>
+      <th>White</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Long-sleeve</td>
+      <td>19$</td>
+      <td>17$</td>
+      <td>15$</td>
+    </tr>
+    <tr>
+      <td>T-shirt</td>
+      <td>14$</td>
+      <td>12$</td>
+      <td>10$</td>
+    </tr>
+    <tr>
+      <td>Jacket</td>
+      <td>34$</td>
+      <td>32$</td>
+      <td>30$</td>
+    </tr>
+  </tbody>
+</table>
+<a href="home.php">Click here for HomePage</a>
+
+<style>
